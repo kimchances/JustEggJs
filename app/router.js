a0c473dc6ff3a088745c61398420faa07a041e1a
kimchances <kimchances@163.com>
2020-04-20 10:52:21 +0800

    修改路由,没有找到router base配置的地方

--- app/router.js
+++ app/router.js
@@ -1,8 +1,8 @@
 module.exports = app => {
     const { router, controller } = app;
     // 获取首页数据
-    router.get('/', controller.index.index);
+    router.get('/egg/', controller.index.index);
     
     // 获取详情数据
-    router.get('/detail/:id', controller.detail.detail);
+    router.get('/egg/detail/:id', controller.detail.detail);
 }
